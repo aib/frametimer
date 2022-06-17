e0b81bfab08b571ce5e69c18d2bbc5ca3cfc0e8c
aib <aibok42@gmail.com>
2022-06-17 16:53:03 +0300

    Factor out FrameTimer.accurate_sleep_until and time_between_frames

--- src/frametimer/frametimer.py
+++ src/frametimer/frametimer.py
@@ -4,6 +4,7 @@ class FrameTimer:
 	def __init__(self, target_fps=60, spin_time=0.001):
 		self.target_fps = target_fps
 		self.spin_time = spin_time
+		self.time_between_frames = 1. / self.target_fps
 		self.last_tick_time = None
 
 	def tick(self):
@@ -11,18 +12,20 @@ class FrameTimer:
 			self.last_tick_time = self.now()
 			return 0.0
 
-		next_tick_time = self.last_tick_time + (1 / self.target_fps)
+		self.accurate_sleep_until(self.last_tick_time + self.time_between_frames)
 
-		sleep_time = next_tick_time - self.now() - self.spin_time
+		now = self.now()
+		dt = now - self.last_tick_time
+		self.last_tick_time = now
+		return dt
+
+	def accurate_sleep_until(self, target_time):
+		sleep_time = target_time - self.now() - self.spin_time
 		if sleep_time > 0:
 			self.sleep(sleep_time)
 
-		while True:
-			now = self.now()
-			if now >= next_tick_time:
-				dt = now - self.last_tick_time
-				self.last_tick_time = now
-				return dt
+		while self.now() < target_time:
+			pass
 
 	def sleep(self, seconds):
 		time.sleep(seconds)
